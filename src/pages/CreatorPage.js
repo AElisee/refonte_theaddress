019c1391e6017330dcac77551fe633f87b816331
AElisee <2kange.eli@gmail.com>
2023-03-03 23:50:37 +0000

    project setup

---
+++ src/pages/CreatorPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatorPage = () => {
+  return (
+    <div>
+      <h1>CreatorPage</h1>
+    </div>
+  );
+};
+
+export default CreatorPage;
---
+++ src/pages/NewProductsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewProductsPage = () => {
+  return (
+    <div>
+      <h1>NewProductsPage</h1>
+    </div>
+  );
+};
+
+export default NewProductsPage;
